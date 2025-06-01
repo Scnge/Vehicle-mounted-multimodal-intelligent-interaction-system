ab1233de4aed9bc71c464390b1b8f74f786544cf
Scnge <127945499+Scnge@users.noreply.github.com>
2025-06-01 17:40:29 +0800

    integrated welcome

--- SoftwareProject/integrated_view/urls.py
+++ SoftwareProject/integrated_view/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.integrated_home, name='integrated_home'),
+    path('welcome/', views.integrated_home, name='integrated_home'),
     path('launch/', views.launch_demo, name='launch_demo'),
     path('status/', views.get_detection_data, name='get_detection_data'),
 ] 
